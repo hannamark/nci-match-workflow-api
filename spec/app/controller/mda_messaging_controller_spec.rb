a9d17090c637357e5a3f68b99ac81cb063417f9f
imativo <isaac.mativo@nih.gov>
2016-01-29 15:51:33 -0500

    unit tests for mda_messaging_controller_spec

--- spec/app/controller/mda_messaging_controller_spec.rb
+++ spec/app/controller/mda_messaging_controller_spec.rb
@@ -1,5 +1,29 @@
+require "#{File.dirname(__FILE__)}/../../../app/controller/application_controller"
 require "#{File.dirname(__FILE__)}/../../../app/controller/mda_messaging_controller"
 require "#{File.dirname(__FILE__)}/../../spec_helper"
 require 'rack/test'
 require 'sinatra'
 
+RSpec.describe Routes::MDAMessagingController do
+
+  def app
+    Routes::MDAMessagingController
+  end
+
+  it 'Fails with Not Implemented status' do
+    post '/assayMessage'
+    expect(last_response.status).to eq 501
+  end
+
+  it 'Fails with Not Implemented status' do
+    post '/setBiopsySpecimenDetails'
+    expect(last_response.status).to eq 501
+  end
+
+  it 'Fails with Not Implemented status' do
+    post '/setNucleicAcidsShippingDetails'
+    expect(last_response.status).to eq 501
+  end
+
+  
+end
