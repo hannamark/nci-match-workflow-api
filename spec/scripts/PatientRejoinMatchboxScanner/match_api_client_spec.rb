ca24a52764e3891959fe4ea12623ebb105fb9909
futo82 <tony.fu@nih.gov>
2016-01-29 19:46:52 -0500

    fix broken rspec tests for MatchAPIClient

--- spec/scripts/PatientRejoinMatchboxScanner/match_api_client_spec.rb
+++ spec/scripts/PatientRejoinMatchboxScanner/match_api_client_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe MatchAPIClient, '#simulate_patient_assignment' do
 
   context 'with valid patient sequence number and analysis id but using default api config' do
     it 'should receive a successful response' do
-      stub_request(:get, 'http://127.0.0.1:8080/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345').
+      stub_request(:get, 'http://127.0.0.1:8080/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345&tieBreakerOptions=0').
           with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
           to_return(:status => 200, :body => '[{ "treatmentArmId" : "EAY131-Q", "treatmentArmVersionId" : "2015-08-06", "reason" : "The patient and treatment arm match on variant identifier [ERBB2]. The variant\'s level of evidence for the treatment arm is 1. The treatment arm\'s matching patient variant does not have a allele frequency. The treatment arm\'s current accrued number is 4. As a final tie breaker a randomizer was used to choose one treatment arm from a total of 3 and this one was selected.", "reasonCategory" : "SELECTED" }]', :headers => {})
 
@@ -44,7 +44,7 @@ RSpec.describe MatchAPIClient, '#simulate_patient_assignment' do
 
   context 'with valid patient sequence number and analysis id' do
     it 'should receive a successful response' do
-      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345').
+      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345&tieBreakerOptions=0').
           with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
           to_return(:status => 200, :body => '[{ "treatmentArmId" : "EAY131-Q", "treatmentArmVersionId" : "2015-08-06", "reason" : "The patient and treatment arm match on variant identifier [ERBB2]. The variant\'s level of evidence for the treatment arm is 1. The treatment arm\'s matching patient variant does not have a allele frequency. The treatment arm\'s current accrued number is 4. As a final tie breaker a randomizer was used to choose one treatment arm from a total of 3 and this one was selected.", "reasonCategory" : "SELECTED" }]', :headers => {})
 
@@ -58,7 +58,7 @@ RSpec.describe MatchAPIClient, '#simulate_patient_assignment' do
 
   context 'with valid patient sequence number and analysis id but Matchbox is down' do
     it 'should receive a internal server error' do
-      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345').
+      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345&tieBreakerOptions=0').
           with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
           to_return(:status => 500, :body => '', :headers => {})
 
@@ -69,7 +69,7 @@ RSpec.describe MatchAPIClient, '#simulate_patient_assignment' do
 
   context 'with valid patient sequence number and analysis id but the endpoint does not exist' do
     it 'should receive a resource not found error' do
-      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345').
+      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345&tieBreakerOptions=0').
           with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
           to_return(:status => 404, :body => '', :headers => {})
 
@@ -80,7 +80,7 @@ RSpec.describe MatchAPIClient, '#simulate_patient_assignment' do
 
   context 'with valid patient sequence number and analysis id but the request bad' do
     it 'should receive a bad request error' do
-      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345').
+      stub_request(:get, 'https://matchhost/match/common/rs/simulateAssignmentByPatient?analysisId=job1&patientId=12345&tieBreakerOptions=0').
           with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'User-Agent'=>'Ruby'}).
           to_return(:status => 400, :body => '', :headers => {})
 
