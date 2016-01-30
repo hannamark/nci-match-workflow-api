733cf16ac3b04710081dc57a5f0407f4144a4dab
futo82 <tony.fu@nih.gov>
2016-01-29 20:29:40 -0500

    fix broken rspec tests for the clia messaging controller

--- app/controller/clia_messaging_controller.rb
+++ app/controller/clia_messaging_controller.rb
@@ -2,18 +2,16 @@ module Routes
   class CLIAMessagingController < ApplicationController
 
     post '/ionReporterUploadFileSet' do
-      "ionReporterUploadFileSet accessed"
-          # status 501
+      status 501
     end
 
     post '/irUploaderHeartBeat' do
-          # status 501
+      status 501
     end
 
     post '/loadPositiveControls' do
-          # status 501
+      status 501
     end
-
 
   end
 end
--- spec/app/controller/clia_messaging_controller_spec.rb
+++ spec/app/controller/clia_messaging_controller_spec.rb
@@ -10,46 +10,19 @@ RSpec.describe Routes::CLIAMessagingController do
     Routes::CLIAMessagingController
   end
 
-
-  it 'Fails if MSN is not provided' do
-
-    get '/assignmentReport?job_id=testjob1'
-
-    expect(last_response.status).to eq 400
-
+  it 'Fails with Not Implemented status' do
+    post '/ionReporterUploadFileSet'
+    expect(last_response.status).to eq 501
   end
 
-  it 'Fail if Job_Id is not provided' do
-
-    get '/variantReport?msn=10369_1000_N-15-00005'
-
-    expect(last_response.status).to eq 400
+  it 'Fails with Not Implemented status' do
+    post '/irUploaderHeartBeat'
+    expect(last_response.status).to eq 501
   end
 
-  it 'Fail if no patient document is found' do
-
-    get '/assignmentReport/?msn=10368_1000_N-15-00005'
-
-    expect(last_response.status).to eq 404
+  it 'Fails with Not Implemented status' do
+    post '/loadPositiveControls'
+    expect(last_response.status).to eq 501
   end
-
-  # it 'Pass if patient document is found' do
-  #
-  #   get '/assignmentReport?msn=10368_1000_N-15-00005&job_id=testjob1'
-  #
-  #   expect(last_response.status).to eq 200
-  # end
-
-
-  context 'Get the test message' do
-    it 'should pass' do
-
-      get '/reportTest'
-
-      expect(last_response.status).to eq 200
-      expect(last_response.body).to include('report')
-    end
-  end
-
 
 end
