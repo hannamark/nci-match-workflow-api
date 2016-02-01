7d2905056b6b0f41dd4a59803890a46e274e38d5
futo82 <tony.fu@nih.gov>
2016-02-01 15:32:27 -0500

    add skeleton spec file for the config helper and simulate assignment analyzer class

---
+++ spec/scripts/PatientRejoinMatchboxScanner/config_helper_spec.rb
@@ -0,0 +1,5 @@
+require "#{File.dirname(__FILE__)}/../../../scripts/PatientRejoinMatchboxScanner/lib/config_helper"
+
+RSpec.describe ConfigHelper, '.get_prop' do
+
+end
---
+++ spec/scripts/PatientRejoinMatchboxScanner/simulate_assignment_analyzer_spec.rb
@@ -0,0 +1,5 @@
+require "#{File.dirname(__FILE__)}/../../../scripts/PatientRejoinMatchboxScanner/lib/simulate_assignment_analyzer"
+
+RSpec.describe ConfigHelper, '.analyze' do
+
+end
