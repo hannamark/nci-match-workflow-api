186e276c95e4f0022b6a71389803c82995470c66
futo82 <tony.fu@nih.gov>
2016-02-03 09:48:19 -0500

    add additional rpsec tests for the SimulateAssignmentAnalyzer to test the case when eligible arms are found

--- spec/scripts/PatientRejoinMatchboxScanner/simulate_assignment_analyzer_spec.rb
+++ spec/scripts/PatientRejoinMatchboxScanner/simulate_assignment_analyzer_spec.rb
@@ -22,10 +22,11 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
       is_eligible, updated_off_trial_patient = analyzer.analyze
       expect(is_eligible).to eq(false)
       expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(1)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][0][:eligibleArms].size).to eq(0)
     end
   end
 
-  context 'simulated assignment have no eligible arms and the latest patient rejoin trigger is not empty' do
+  context 'simulated assignment have no eligible arms and the latest patient rejoin trigger contains no eligible arms' do
     it 'should return false and the latest rejoin trigger scanned date should be updated' do
       off_trial_patient = {
           :patientRejoinTriggers => [{
@@ -48,6 +49,7 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
       is_eligible, updated_off_trial_patient = analyzer.analyze
       expect(is_eligible).to eq(false)
       expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(1)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][0][:eligibleArms].size).to eq(0)
     end
   end
 
@@ -77,6 +79,90 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
       is_eligible, updated_off_trial_patient = analyzer.analyze
       expect(is_eligible).to eq(false)
       expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(2)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][1][:eligibleArms].size).to eq(0)
+    end
+  end
+
+  context 'simulated assignment have eligible arms and the latest patient trigger is empty' do
+    it 'should return true and the updated patient object contains a new rejoin trigger' do
+      off_trial_patient = {
+          :patientRejoinTriggers => []
+      }
+
+      assignment_results = {
+          'results' => [
+              {
+                  'treatmentArmId' => 'ArmA',
+                  'treatmentArmVersion' => 'v1',
+                  'reasonCategory' => 'SELECTED'
+              }
+          ]
+      }
+
+      analyzer = SimulateAssignmentAnalyzer.new(off_trial_patient, assignment_results)
+      is_eligible, updated_off_trial_patient = analyzer.analyze
+      expect(is_eligible).to eq(true)
+      expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(1)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][0][:eligibleArms].size).to eq(1)
+    end
+  end
+
+  context 'simulated assignment have eligible arms and the latest patient trigger contains no eligible arms' do
+    it 'should return true and the updated patient object contains a new rejoin trigger' do
+      off_trial_patient = {
+        :patientRejoinTriggers => [{
+          :eligibleArms => [],
+          :dateScanned => DateTime.now
+        }]
+      }
+
+      assignment_results = {
+          'results' => [
+              {
+                  'treatmentArmId' => 'ArmA',
+                  'treatmentArmVersion' => 'v1',
+                  'reasonCategory' => 'SELECTED'
+              }
+          ]
+      }
+
+      analyzer = SimulateAssignmentAnalyzer.new(off_trial_patient, assignment_results)
+      is_eligible, updated_off_trial_patient = analyzer.analyze
+      expect(is_eligible).to eq(true)
+      expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(2)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][1][:eligibleArms].size).to eq(1)
+    end
+  end
+
+  context 'simulated assignment have eligible arms and the latest patient trigger contains a rejoined date' do
+    it 'should return true and the updated patient object contains a new rejoin trigger' do
+      off_trial_patient = {
+        :patientRejoinTriggers => [{
+          :eligibleArms => [{
+            :treatmentArmId => 'ArmB',
+            :treatmentArmVersion => 'v1'
+          }],
+          :dateScanned => DateTime.now,
+          :dateSentToECOG => DateTime.now,
+          :dateRejoined => DateTime.now
+        }]
+      }
+
+      assignment_results = {
+          'results' => [
+              {
+                  'treatmentArmId' => 'ArmA',
+                  'treatmentArmVersion' => 'v1',
+                  'reasonCategory' => 'SELECTED'
+              }
+          ]
+      }
+
+      analyzer = SimulateAssignmentAnalyzer.new(off_trial_patient, assignment_results)
+      is_eligible, updated_off_trial_patient = analyzer.analyze
+      expect(is_eligible).to eq(true)
+      expect(updated_off_trial_patient[:patientRejoinTriggers].size).to eq(2)
+      expect(updated_off_trial_patient[:patientRejoinTriggers][1][:eligibleArms].size).to eq(1)
     end
   end
 
