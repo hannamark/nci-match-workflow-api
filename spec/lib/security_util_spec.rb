9eda4d0a32b5b7201bc62df40923ee8e649040bd
imativo <isaac.mativo@nih.gov>
2016-02-08 16:16:43 -0500

    spec file for security util

---
+++ spec/lib/security_util_spec.rb
@@ -0,0 +1,31 @@
+require "#{File.dirname(__FILE__)}/../spec_helper"
+require "#{File.dirname(__FILE__)}/../../lib/security_util"
+require 'rspec'
+
+include SecurityUtil
+
+RSpec.describe SecurityUtil, '#encrypt string' do
+
+
+    before {
+      @password_string = 'password'
+      @encrypted_string = SecurityUtil::Aes.encrypt(@password_string)
+      @decrypted_string = SecurityUtil::Aes.decrypt(@encrypted_string)
+    }
+
+  context 'with a password string' do
+
+    it 'should not return the same password string' do
+      p @password_string
+      p @encrypted_string
+      p @decrypted_string
+      expect @encrypted_string != @password_string
+    end
+
+    it 'should decrypt and return the original string' do
+      expect @decrypted_string == @password_string
+    end
+
+  end
+
+end
