dacff1ba595a6352892b6cd82f55cc453eb258bf
josephverbeck <joseph.verbeck@nih.gov>
2016-02-22 17:20:06 -0500

    Adding in the decrypt function for get_value

--- scripts/PatientRejoinMatchboxScanner/lib/match_properties_message_dao.rb
+++ scripts/PatientRejoinMatchboxScanner/lib/match_properties_message_dao.rb
@@ -1,10 +1,12 @@
 require 'mongo'
 
 require "#{File.dirname(__FILE__)}/config_helper"
+require "#{File.dirname(__FILE__)}/../../../lib/security_util"
+
 
 class MatchPropertiesMessageDao
 
-  def initialize(db_config)
+  def initialize()
     @hosts = ConfigHelper.get_prop(db_config, 'database', 'hosts', ['127.0.0.1:27017'])
     @dbname = ConfigHelper.get_prop(db_config, 'database', 'dbname', 'match')
     @username = ConfigHelper.get_prop(db_config, 'database', 'username', nil)
@@ -14,7 +16,10 @@ class MatchPropertiesMessageDao
   end
 
   def get_value(prop_key)
-    @client[:matchPropertiesMessage].find(:_id => prop_key).limit(1)
+    @client[:matchPropertiesMessage].find(:_id => prop_key).limit(1).each do | doc |
+      return SecurityUtil::Aes.decrypt(doc[:value].as_json["$binary"])
+    end
+
   end
 
 end
