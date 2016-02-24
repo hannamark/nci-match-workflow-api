d0d0b2856f3be0a88455ea42bb27ba0538e7c330
imativo <isaac.mativo@nih.gov>
2016-02-24 16:56:01 -0500

    added unit tests and initialize param for match_properties_message_dao

--- scripts/PatientRejoinMatchboxScanner/lib/match_properties_message_dao.rb
+++ scripts/PatientRejoinMatchboxScanner/lib/match_properties_message_dao.rb
@@ -6,7 +6,7 @@ require "#{File.dirname(__FILE__)}/../../../lib/security_util"
 
 class MatchPropertiesMessageDao
 
-  def initialize()
+  def initialize(db_config)
     @hosts = ConfigHelper.get_prop(db_config, 'database', 'hosts', ['127.0.0.1:27017'])
     @dbname = ConfigHelper.get_prop(db_config, 'database', 'dbname', 'match')
     @username = ConfigHelper.get_prop(db_config, 'database', 'username', nil)
@@ -17,7 +17,7 @@ class MatchPropertiesMessageDao
 
   def get_value(prop_key)
     @client[:matchPropertiesMessage].find(:_id => prop_key).limit(1).each do | doc |
-      return SecurityUtil::Aes.decrypt(doc[:value].as_json["$binary"])
+      return SecurityUtil::AES.decrypt(doc[:value].as_json["$binary"])
     end
 
   end
