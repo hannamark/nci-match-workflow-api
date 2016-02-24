83dbecf6c375ba1c3d529c5d261ab06413535e6f
josephverbeck <joseph.verbeck@nih.gov>
2016-02-24 15:33:01 -0500

    Adding in check for blank

--- scripts/PatientRejoinMatchboxScanner/lib/config_helper.rb
+++ scripts/PatientRejoinMatchboxScanner/lib/config_helper.rb
@@ -1,6 +1,6 @@
 class ConfigHelper
   def self.get_prop(config, resource_key, prop_key, default_value)
-    if !config.nil? && config.has_key?(resource_key) && config[resource_key].has_key?(prop_key)
+    if !config.blank? && config.has_key?(resource_key) && config[resource_key].has_key?(prop_key)
       return config[resource_key][prop_key]
     end
     return default_value
