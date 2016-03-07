e11c180c09ede9ce1d2cee85a8427f0aae82453d
josephverbeck <joseph.verbeck@nih.gov>
2016-03-07 12:38:47 -0500

    Fixing lazy initialize

--- scripts/PatientRejoinMatchboxScanner/lib/rejoin_logger.rb
+++ scripts/PatientRejoinMatchboxScanner/lib/rejoin_logger.rb
@@ -7,13 +7,17 @@ module RejoinLogger
   class << self
     attr_accessor :logger
 
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
     def logger=(logger)
-      @logger = logger
+      @logger ||= logger
       Mongo::Logger.logger = @logger
     end
 
     def level=(level)
-      @logger.level = level
+      @logger.level ||= level
       Mongo::Logger.logger.level = @logger.level
     end
   end
