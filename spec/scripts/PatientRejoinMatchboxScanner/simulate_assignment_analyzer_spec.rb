a3c5b12e2f5f13b8c3458a6958e6b472bb1cb0f9
futo82 <tony.fu@nih.gov>
2017-01-13 15:24:04 -0500

    Add patient triggers to the SimulateAssignmentAnalyzer rspec tests

--- spec/scripts/PatientRejoinMatchboxScanner/simulate_assignment_analyzer_spec.rb
+++ spec/scripts/PatientRejoinMatchboxScanner/simulate_assignment_analyzer_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have no eligible arms and patient rejoin trigger is empty' do
     it 'should return false and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
+        'patientTriggers' => [],
         'patientRejoinTriggers' => []
       }
 
@@ -29,6 +30,7 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have no eligible arms and the latest patient rejoin trigger contains no eligible arms' do
     it 'should return false and the latest rejoin trigger scanned date should be updated' do
       off_trial_patient = {
+          'patientTriggers' => [],
           'patientRejoinTriggers' => [{
             'eligibleArms' => [],
             'dateScanned' => DateTime.now
@@ -56,6 +58,7 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have no eligible arms and the latest patient rejoin trigger contains a eligible arm' do
     it 'should return false and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
+          'patientTriggers' => [],
           'patientRejoinTriggers' => [{
             'eligibleArms' => [{
               'treatmentArmId' => 'ArmB',
@@ -86,7 +89,8 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have eligible arms and the latest patient trigger is empty' do
     it 'should return true and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
-        'patientRejoinTriggers' => []
+          'patientTriggers' => [],
+          'patientRejoinTriggers' => []
       }
 
       assignment_results = {
@@ -110,10 +114,11 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have eligible arms and the latest patient trigger contains no eligible arms' do
     it 'should return true and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
-        'patientRejoinTriggers' => [{
-          'eligibleArms' => [],
-          'dateScanned' => DateTime.now
-        }]
+          'patientTriggers' => [],
+          'patientRejoinTriggers' => [{
+            'eligibleArms' => [],
+            'dateScanned' => DateTime.now
+          }]
       }
 
       assignment_results = {
@@ -137,15 +142,16 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment have eligible arms and the latest patient trigger contains a rejoined date' do
     it 'should return true and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
-        'patientRejoinTriggers' => [{
-          'eligibleArms' => [{
-            'treatmentArmId' => 'ArmB',
-            'treatmentArmVersion' => 'v1'
-          }],
-          'dateScanned' => DateTime.now,
-          'dateSentToECOG' => DateTime.now,
-          'dateRejoined' => DateTime.now
-        }]
+          'patientTriggers' => [],
+          'patientRejoinTriggers' => [{
+            'eligibleArms' => [{
+              'treatmentArmId' => 'ArmB',
+              'treatmentArmVersion' => 'v1'
+            }],
+            'dateScanned' => DateTime.now,
+            'dateSentToECOG' => DateTime.now,
+            'dateRejoined' => DateTime.now
+          }]
       }
 
       assignment_results = {
@@ -169,14 +175,15 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment found different eligible arms and the latest patient trigger does not have date rejoined' do
     it 'should return true and the updated patient object contains a new rejoin trigger' do
       off_trial_patient = {
-        'patientRejoinTriggers' => [{
-          'eligibleArms' => [{
-            'treatmentArmId' => 'ArmA',
-            'treatmentArmVersion' => 'v1'
-          }],
-          'dateScanned' => DateTime.now,
-          'dateSentToECOG' => DateTime.now
-        }]
+          'patientTriggers' => [],
+          'patientRejoinTriggers' => [{
+            'eligibleArms' => [{
+              'treatmentArmId' => 'ArmA',
+              'treatmentArmVersion' => 'v1'
+            }],
+            'dateScanned' => DateTime.now,
+            'dateSentToECOG' => DateTime.now
+          }]
       }
 
       assignment_results = {
@@ -203,14 +210,15 @@ RSpec.describe SimulateAssignmentAnalyzer, '#analyze' do
   context 'simulated assignment did not find different eligible arms and the latest patient trigger does not have date rejoined' do
     it 'should return true and the latest rejoin trigger scanned date should be updated' do
       off_trial_patient = {
-        'patientRejoinTriggers' => [{
-          'eligibleArms' => [{
-            'treatmentArmId' => 'ArmA',
-            'treatmentArmVersion' => 'v1'
-          }],
-          'dateScanned' => DateTime.now,
-          'dateSentToECOG' => DateTime.now
-        }]
+          'patientTriggers' => [],
+          'patientRejoinTriggers' => [{
+            'eligibleArms' => [{
+              'treatmentArmId' => 'ArmA',
+              'treatmentArmVersion' => 'v1'
+            }],
+            'dateScanned' => DateTime.now,
+            'dateSentToECOG' => DateTime.now
+          }]
       }
 
       assignment_results = {
